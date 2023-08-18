5f8776d4708b53a4ea10106bb6c47c736363d0af
Jonatas Santos <jonatassantos96@hotmail.com>
2023-08-18 15:35:49 -0300

    ✨ Show data list from database on home screen

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { LoadingController } from '@ionic/angular';
+
+interface Player {
+  id: string;
+  name: string;
+  wins: string;
+  years: string;
+  photo: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +17,52 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public players: Player[] = [];
+  public isPlayers: boolean = false;
 
-  constructor() {}
+  constructor(
+    private http: HttpClient,
+    private loadingCtrl: LoadingController
+  ) {}
 
+  async ngOnInit(): Promise<void> {
+    const loading = await this.loadingCtrl.create({
+      message: 'Carregando...',
+      duration: 2000,
+    });
+
+    try {
+      loading.present();
+      this.getPlayers('');
+
+      await new Promise((f) => setTimeout(f, 2000));
+      this.isPlayers = true;
+    } catch (error) {
+      console.log(error);
+      await loading.dismiss();
+    }
+  }
+
+  ionViewDidEnter() {}
+
+  handleInput(event: any) {
+    const searchText = event.target.value;
+
+    this.getPlayers(searchText);
+  }
+
+  getPlayers(search: string) {
+    const params = new HttpParams().set('search', search);
+
+    this.http
+      .get('http://localhost/crud-angular-mysql/backend/players.php', {
+        params,
+        observe: 'response',
+      })
+      .subscribe((response) => {
+        console.log(response);
+
+         this.players = response.body as Player[];
+      });
+  }
 }
