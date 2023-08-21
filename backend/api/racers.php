7c67a3a4eac9322118889e9b68af34a03bb1f76a
Jonatas Santos <jonatassantos96@hotmail.com>
2023-08-21 14:52:15 -0300

    ♻️ Refactor racers api

---
+++ backend/api/racers.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "../connection.php";
+
+include_once "../controllers/racerController.php";
+
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case 'GET':
+        if (isset($_GET['search'])) {
+            $searchText = $_GET['search'];
+            $raceController = RacerController::getInstance($connection);
+
+            $racers = $raceController->searchRacers($searchText);
+
+            echo json_encode($racers);
+        } else {
+            $raceController = RacerController::getInstance($connection);
+            $racers = $raceController->getRacers();
+
+            echo json_encode($racers);
+        }
+        break;
+
+    case 'POST':
+        # code...
+        break;
+
+    case 'PUT':
+        # code...
+        break;
+
+    case 'DELETE':
+        # code...
+        break;
+}
